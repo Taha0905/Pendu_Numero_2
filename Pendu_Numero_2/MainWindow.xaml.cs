676640d80a427b8cf26abb731be846295eeb506e
taha khelifi <taha.khelifi@saintmichelannecy.fr>
2023-10-22 17:14:30 +0200

    2.1

--- Pendu_Numero_2/MainWindow.xaml.cs
+++ Pendu_Numero_2/MainWindow.xaml.cs
@@ -29,10 +29,63 @@ namespace Pendu_Numero_2
             // Remplir les listes de pays et de capitales (en majuscules)
             pays.Add("FRANCE");
             pays.Add("ESPAGNE");
+            pays.Add("ITALIE");
+            pays.Add("ALLEMAGNE");
+            pays.Add("PORTUGAL");
+            pays.Add("BELGIQUE");
+            pays.Add("SUISSE");
+            pays.Add("ROYAUME-UNI");
+            pays.Add("AUTRICHE");
+            pays.Add("IRLANDE");
+            pays.Add("SUEDE");
+            pays.Add("DANEMARK");
+            pays.Add("FINLANDE");
+            pays.Add("NORVEGE");
+            pays.Add("GRECE");
+            pays.Add("TURQUIE");
+            pays.Add("POLOGNE");
+            pays.Add("TCHEQUIE");
+            pays.Add("HONGRIE");
+            pays.Add("ROUMANIE");
+            pays.Add("BULGARIE");
+            pays.Add("CROATIE");
+            pays.Add("SERBIE");
+            pays.Add("SLOVENIE");
+            pays.Add("SLOVAQUIE");
+            pays.Add("UKRAINE");
+            pays.Add("BIELORUSSIE");
+            pays.Add("RUSSIE");
+            pays.Add("ESTONIE");
+            pays.Add("LETTONIE");
+            pays.Add("LITUANIE");
+            pays.Add("MOLDAVIE");
             // Ajoutez d'autres pays ici...
 
             capitales.Add("PARIS");
             capitales.Add("MADRID");
+            capitales.Add("ROME");
+            capitales.Add("BERLIN");
+            capitales.Add("LISBONNE");
+            capitales.Add("BRUXELLES");
+            capitales.Add("BERNE");
+            capitales.Add("LONDRES");
+            capitales.Add("VIENNE");
+            capitales.Add("DUBLIN");
+            capitales.Add("STOCKHOLM");
+            capitales.Add("COPENHAGUE");
+            capitales.Add("HELSINKI");
+            capitales.Add("OSLO");
+            capitales.Add("ATHENES");
+            capitales.Add("ANKARA");
+            capitales.Add("VARSOVIE");
+            capitales.Add("PRAGUE");
+            capitales.Add("BUDAPEST");
+            capitales.Add("BUCHAREST");
+            capitales.Add("SOFIA");
+            capitales.Add("ZAGREB");
+            capitales.Add("BELGRADE");
+            capitales.Add("LJUBLJANA");
+            capitales.Add("BRATISLAVA");
             // Ajoutez d'autres capitales ici...
 
             // Désactiver tous les boutons du clavier au démarrage
